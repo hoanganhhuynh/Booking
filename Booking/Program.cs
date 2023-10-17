879642370d1a973b155f6ab17b330c7af30b02bd
Hoang Anh <anhhuynh@Vuongs-MacBook-Pro.local>
2023-10-17 08:51:55 +0700

    Fix bug

--- Booking/Program.cs
+++ Booking/Program.cs
@@ -17,11 +17,10 @@ public class Program
         var action = "";
         do
         {
-            Console.Write("Please enter number of guests");
+            Console.Write("Please enter number of guests: ");
             int numberPerson = 0;
             bool isValid = int.TryParse(Console.ReadLine(), out numberPerson);
 
-            // Loops until user enters number from 1 - 7
             while (!isValid || numberPerson == 0)
             {
                 
@@ -34,6 +33,7 @@ public class Program
             {
 
                 var result = GetBookingConfirmation(numberPerson, reservationService);
+                Console.WriteLine(result);
             }
             else
             {
@@ -41,20 +41,20 @@ public class Program
             }
 
             Console.Write("Do you want to continue booking? Enter Y if you want to continue. Enter any character to stop booking.");
+            action = Console.ReadLine();
         }
-        while (action == "Y");
+        while (action?.ToUpper() == "Y");
 
         
     }
 
     private static string GetBookingConfirmation(int numberPerson, IHotelReservation hotelReservation)
     {
-        var hasAvailableRoom = hotelReservation.IsEnoughRoom(7);
+        var hasAvailableRoom = hotelReservation.IsEnoughRoom(numberPerson);
         if (hasAvailableRoom)
         {
-            var reservations = hotelReservation.CteateReservation(7);
+            var reservations = hotelReservation.CteateReservation(numberPerson);
             return $"{string.Join(" ", reservations.Select(r => r.RoomType))} {reservations.Select(r => r.Price).Sum()}";
-            
         }
         else
         {
